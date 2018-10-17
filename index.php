0bdd6080023252a98fc927bec2fa1e72e1bfb888
Anthony <huasipango@gmail.com>
2018-10-16 22:16:37 -0500

    interfaz para trabajo

--- index.php
+++ index.php
@@ -7,8 +7,37 @@
 </head>
 <body>
 	<header >
-		
+		<article class="jumbotron" style="background-color:#0747a6">
+			<h1 class="text-center" style="color: white"> Calculadora de Subredes </h1>
+		</article>
 	</header>
+	<article class="container">
+		<form action="index.php" method="POST">
+			<div  class="form-group row">
+				<div class="form-group form-group-sm col-sm-4">
+					<label class="col-form-label">Dirección IP:</label>	
+					<input type="text" name="ipAdress" value="192.168.0.0" class="form-control">		
+				</div>
+				<div class="form-group form-group-sm col-sm-4">
+					<label class="col-form-label">Netmask Inicial (ejem: 24):</label>
+					<input type="number" name="firstNetmask" value="24" class="form-control">		
+				</div>
+				<div class="form-group form-group-sm col-sm-4">
+					<label class="col-form-label">Netmask Final (ejem: 30):</label>		
+					<input type="number" name="finalNetmask" class="form-control">		
+				</div>
+			</div>
+			<div class="text-center">
+				<button type="submit" class="btn btn-primary">
+					Calcular
+				</button>
+				<button class="btn btn-success">
+					Limpiar
+				</button>
+			</div>
+			
+		</form>
+	</article>
 	
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
