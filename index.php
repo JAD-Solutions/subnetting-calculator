1a99140747de1121286359a89727507293f3a712
Anthony <huasipango@gmail.com>
2018-10-16 23:44:32 -0500

    trabajando con variables correcto

--- index.php
+++ index.php
@@ -11,20 +11,44 @@
 			<h1 class="text-center" style="color: white"> Calculadora de Subredes </h1>
 		</article>
 	</header>
+
+    <?php
+    if(isset($_POST["ipAdress"]))
+    {
+        $ipAdress = $_POST["ipAdress"];
+    }
+    else{
+        $ipAdress= 0;
+    }
+    if(isset($_POST["firstNetmask"]))
+    {
+        $firstNetmask= $_POST["firstNetmask"];
+    }
+    else{
+        $firstNetmask = 0;
+    }
+    if(isset($_POST["finalNetmask"]))
+    {
+        $finalNetmask= $_POST["finalNetmask"];
+    }
+    else{
+        $finalNetmask= 0;
+    }
+    ?> <!--Validación variables cargadas-->
 	<article class="container">
 		<form action="index.php" method="POST">
 			<div  class="form-group row">
 				<div class="form-group form-group-sm col-sm-4">
 					<label class="col-form-label">Dirección IP:</label>	
-					<input type="text" name="ipAdress" value="192.168.0.0" class="form-control">		
+					<input type="text" name="ipAdress" value="<?php echo $ipAdress; ?>" class="form-control">
 				</div>
 				<div class="form-group form-group-sm col-sm-4">
 					<label class="col-form-label">Netmask Inicial (ejem: 24):</label>
-					<input type="number" name="firstNetmask" value="24" class="form-control">		
+					<input type="number" name="firstNetmask" value="<?php echo $firstNetmask; ?>" class="form-control">
 				</div>
 				<div class="form-group form-group-sm col-sm-4">
 					<label class="col-form-label">Netmask Final (ejem: 30):</label>		
-					<input type="number" name="finalNetmask" class="form-control">		
+					<input type="number" name="finalNetmask" value="<?php echo $finalNetmask; ?>" class="form-control">
 				</div>
 			</div>
 			<div class="text-center">
@@ -38,7 +62,7 @@
 			
 		</form>
 	</article>
-	
+
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
